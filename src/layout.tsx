686942a97855a5fd74843f53c9c7021c87ec00b0
Cédric Marcone <cedric.marcone@valraiso.fr>
2022-11-10 08:55:01 +0100

    working second layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import useSize from "@react-hook/size";
 import classNames from "classnames";
 import List from "./list";
 import Map from "./map";
@@ -9,15 +10,28 @@ type Props = {
 };
 
 export default function Layout({ dialog = false }: Props) {
+  const outerRef = React.useRef<HTMLDivElement>(null);
+  const listRef = React.useRef<HTMLDivElement>(null);
+  const mapRef = React.useRef<HTMLDivElement>(null);
+
+  const outerSize = useSize(outerRef);
+  const listSize = useSize(listRef);
+  const mapSize = useSize(mapRef);
+
   const outerClasses = classNames(css.outer, { [css.dialog]: dialog });
   const mapClasses = classNames(css.map, { [css.dialog]: dialog });
   return (
-    <div className={outerClasses}>
-      <div className={css.list}>
+    <div className={outerClasses} ref={outerRef}>
+      <div className={css.list} ref={listRef}>
         <List />
       </div>
       <div className={mapClasses}>
-        <Map />
+        <Map
+          dialog={dialog}
+          size={mapSize}
+          widgetSize={outerSize}
+          ref={mapRef}
+        />
       </div>
     </div>
   );
--- src/map.tsx
+++ src/map.tsx
@@ -1,6 +1,61 @@
 import * as React from "react";
+import classNames from "classnames";
+import { useDrag } from "@use-gesture/react";
+import { useSpring, animated } from "@react-spring/web";
 import css from "./map.module.css";
 
-export default function Map() {
-  return <div className={css.outer}>Map</div>;
-}
+type Props = {
+  dialog?: boolean;
+  size: [number, number];
+  widgetSize: [number, number];
+};
+
+const BAR_TOP = 75;
+const BAR_BOTTOM = 100;
+
+const Map = React.forwardRef(
+  (
+    { dialog = false, size: [, height], widgetSize: [width] }: Props,
+    ref: any
+  ) => {
+    const [{ y }, api] = useSpring(() => ({ y: 0 }));
+
+    React.useEffect(() => {
+      const appMode = dialog && width < 600;
+      api.set({ y: appMode ? height - BAR_TOP : 0 });
+    }, [api, dialog, width, height]);
+
+    const bind = useDrag((state) => {
+      const [, my] = state.movement;
+      const [, iy] = state.initial;
+      const [, dy] = state.offset;
+
+      const y = iy + my;
+      const start = height - BAR_TOP;
+      const distance = Math.abs(dy);
+      const upward = dy > 0;
+
+      if (state.down) {
+        return api.start({ y });
+      }
+      if (state.tap) {
+        return y < BAR_BOTTOM ? api.start({ y: start }) : api.start({ y: 0 });
+      }
+      return distance > height / 8 && upward
+        ? api.start({ y: start })
+        : api.start({ y: 0 });
+    });
+
+    const outerClasses = classNames(css.outer, { [css.dialog]: dialog });
+    return (
+      <animated.div className={outerClasses} style={{ y }} ref={ref}>
+        <div className={css.dragBar} {...bind()}>
+          <div className={css.drag} />
+        </div>
+        <div className={css.map}>Map</div>
+      </animated.div>
+    );
+  }
+);
+
+export default Map;
